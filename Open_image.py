284fcbd94fcbf6fc07bc4944d17a4a309592cd42
praneshrott <praneshj.18ece@kongu.edu>
2021-03-17 18:06:24 +0530

    changes modified

--- Open_image.py
+++ Open_image.py
@@ -6,7 +6,7 @@ Created on Sun Feb 14 11:02:48 2021
 """
 import numpy as np
 import cv2
-import pickle,os
+import pickle,os#commit added
 img=cv2.imread(r"C:\Users\Pranesh Jayaprakash\Downloads\WhatsApp Image 2021-02-11 at 10.35.09 AM.jpeg",cv2.IMREAD_UNCHANGED)
 cv2.imshow("image",img)
 cv2.waitKey(0)
